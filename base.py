6c2b405fb7a0fba887c823b1954b7d8365be9bd6
Magnesium <mehdighorashi83@gmail.com>
2025-07-13 21:01:00 +0330

    Added ParsTree class to base.py from_string method must be implemented in the next commits

--- base.py
+++ base.py
@@ -29,3 +29,54 @@ class Node:
         if node is None or not isinstance(node, Node):
             raise ValueError("Left child must be a Node instance.")
         self.__left = node
+
+class ParseTree:
+    def __init__(self, formula: str = None) -> None:
+        self.__root: Node = None
+        if formula is not None:
+            self.from_string(formula)
+    
+    def __repr__(self):
+        return f"ParseTree(formula={self.__str__()})"
+    
+    def __str__(self):
+        return "".join(self.__inorder())
+
+    def __inorder(self) -> list[Node]:
+        if not self.is_built:
+            return []
+        result = []
+        def _inorder(node: Node):
+            if node is not None:
+                _inorder(node.left)
+                result.append(node)
+                _inorder(node.right)
+        _inorder(self.__root)
+        return result
+    
+    def preorder(self) -> str:
+        if not self.is_built:
+            return ""
+        
+        result = []
+        
+        def _preorder(node: Node, depth: int = 0):
+            if node is not None:
+                indent = "  " * depth
+                result.append(f"{indent}{node.value}")
+                _preorder(node.left, depth + 1)
+                _preorder(node.right, depth + 1)
+        
+        _preorder(self.__root)
+        return "\n".join(result)
+
+    @property
+    def is_built(self) -> bool:
+        return self.__root is not None
+
+    @property
+    def root(self) -> Node:
+        return self.__root
+    
+    def from_string(self, formula: str) -> "ParseTree":
+        pass
